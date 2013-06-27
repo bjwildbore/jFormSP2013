ddc6f5b22b9340c7acf3842fde5e6a2bb4c344d5
BJ Wildbore <bjwildbore@gmail.com>
2013-06-27 16:33:31 +1200

    begun development of dialogs

--- jquery.jformsp.js
+++ jquery.jformsp.js
@@ -7,14 +7,18 @@
             var $this = $(this),
 			settings = $.extend({			   
 				
-			    "afterInit": function () { return true; },
+			    "afterInit": function () { console.log('afterInit');return true; },
 			    "afterListParse": function () { return true; },
-/*
+			    "showDialog": function (innerHtml) { return true; },
+			    "closeDialog": function (innerHtml) { return true; },
+
+
+
 			    "onClickAdd": function () { return true; },
 			    "onClickEdit": function () { return true; },
 			    "onClickDelete": function () { return true; },
 			    
-			    "afterDialogShow": function () { return true; },
+			    "afterDialogShow": function () {console.log('afterDialogShow'); return true; },
 				
 			    "afterObjectAdd": function () { return true; },
 			    "afterObjectRemove": function () { return true; },
@@ -23,19 +27,48 @@
 			    "beforeObjectAdd": function () { return true; },
 			    "beforeObjectUpdate": function () { return true; },			    
 			    "beforeObjectRemove": function () { return true; }
-*/				
 			}, options);            
 
 			_parseLists($this, settings);
 			console.log(settings)
-            $this.data("settings", settings);			
+            $this.data("settings", settings);	
+            $this.data('test','test1');		
+
+
 			
             if (typeof settings.afterInit === "function") {
                 if (!settings.afterInit.call(this, $this)) {
                     return false;
                 }
             } 
-        },	
+        },
+        	
+		showForm: function(type,obj){
+			console.log('showEditForm',obj);
+			var $this = $(this),
+				settings = $this.data("settings"),
+				innerHTML = _getFormInnerHTML($this,type,obj);		
+			
+			/*
+			
+				Do Stuff Here
+			
+			*/
+			if (typeof settings.showDialog=== "function") {
+                if (!settings.showDialog.call(this, innerHTML )) {
+                    return false;
+                }
+            }
+			
+			if (typeof settings.afterDialogShow === "function") {
+                if (!settings.afterDialogShow.call(this, $this)) {
+                    return false;
+                }
+            }
+		}
+		
+		
+        	
     };
 	
     $.fn.jfsp = function (method) {
@@ -50,6 +83,91 @@
     };
 
     /* Private functions */
+
+
+
+	function _getFormInnerHTML($this,type,obj){
+		var sReturn = "";
+		
+		switch(type){
+			case ('display'):
+				sReturn = _getDisplayFormInnerHTML($this,obj);
+				break;
+
+			case ('add'):
+				sReturn = _getAddFormInnerHTML($this,obj);
+				break;
+				
+			case ('edit'):
+				sReturn = _getEditFormInnerHTML($this,obj);
+				break;
+			
+			case ('remove'):
+				sReturn = _getRemoveFormInnerHTML($this,obj);
+				break;
+		
+		}
+		
+		return sReturn;
+	}
+
+
+
+	function _getListItem(id, list){
+		var oReturn = {}
+		console.log('_getListItem',id,list);
+		$().SPServices({
+	        operation:	"GetListItems",
+	        async:	false,
+	        listName:	list,
+	        CAMLQuery:	"<Query><Where><Eq><FieldRef Name='ID'/><Value Type='Counter'>"+id+"</Value></Eq></Where></Query>",
+	        CAMLRowLimit: 10,
+	        completefunc: function(xData,status){	        	
+	        	$(xData.responseXML).SPFilterNode("z:row").each(function() { 
+	        		oReturn = _xmlToJson(this)
+	        	});	        	
+	        }
+	    })
+	    console.log(oReturn )
+		return oReturn;
+	}
+
+	/*
+		obj.itemID
+		obj.list
+	*/
+	function _getEditFormInnerHTML($this,obj){
+		var sHtml = 'getEditFormInnerHTML',
+			settings = $this.data('settings'),
+			id = obj.id,			
+			list = settings.lists[obj.list],
+			item = 	_getListItem(id,list.listName);
+		
+			console.log('getEditFormInnerHTML',item,list)
+		
+		return sHtml;
+	}
+
+	function _getAddFormInnerHTML($this){
+		var sHtml = 'getAddFormInnerHTML';
+		
+		return sHtml;
+	}
+	
+	function _getDisplayFormInnerHTML($this){
+		var sHtml = 'getDisplayFormInnerHTML';
+		
+		return sHtml;
+	
+	}
+
+	function _getRemoveFormInnerHTML($this,obj){
+		var sHtml = 'getRemoveFormInnerHTML';
+		
+		return sHtml;
+	
+	}
+
 
 	function _parseLists($this,settings){
 		console.log('_parseLists',settings);
@@ -105,18 +223,17 @@
 	}
 	
 	
-	function _xmlToJson(xml) {
-		
+	function _xmlToJson(xml) {		
 		// Create the return object
 		var obj = {};
 	
 		if (xml.nodeType == 1) { // element
 			// do attributes
 			if (xml.attributes.length > 0) {
-			obj["attributes"] = {};
+				obj["attributes"] = {};
 				for (var j = 0; j < xml.attributes.length; j++) {
 					var attribute = xml.attributes.item(j);
-					obj["attributes"][attribute.nodeName] = attribute.nodeValue;
+					obj["attributes"][attribute.nodeName.replace('ows_','')] = attribute.nodeValue;
 				}
 			}
 		} else if (xml.nodeType == 3) { // text
